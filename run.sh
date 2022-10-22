821c245663a0bb42691bafabcebf5d8be4b7b4d3
shrank <michael.salathe@murxs.ch>
2022-10-22 09:24:36 +0200

    Update run.sh
    auto generate certs

--- run.sh
+++ run.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+mkdir -p /etc/nginx/snippets/
+mkdir -p /etc/nginx/certs/
+cd /etc/nginx/certs/
+
+if [ ! -f cert.pem ] || [ ! -f key.pem ]; then
+  openssl req -subj '/CN=localhost' -x509 -newkey rsa:4096 -nodes -keyout key.pem -out cert.pem -days 365
+fi
+
+if [ ! -f dhparam.pem ] || [ ! -z ${USE_DH+x} ]; then
+ openssl dhparam -out dhparam.pem 4096
+ echo """
+ ssl_dhparam /etc/nginx/certs/dhparam.pem;
+ ssl_ecdh_curve secp384r1;
+ """ > /etc/nginx/snippets/dhparm.conf
+fi
+
 sed -i -e "s|XX_REMOTE_URL_XX|$REMOTE_URL|g" /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
